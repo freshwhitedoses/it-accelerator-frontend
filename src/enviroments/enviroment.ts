74dd8f8628485575a6f649ab64524135ee063981
olechochek <92031827+freshwhitedoses@users.noreply.github.com>
2024-06-11 11:27:48 +0300

    Update enviroment.ts

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  auth: 'http://158.160.87.34:8083',
-  api: 'http://158.160.87.34:8083',
+  auth: 'http://158.160.64.213:8083',
+  api: 'http://158.160.64.213:8083',
 };
